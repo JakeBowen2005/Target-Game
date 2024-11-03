9a2b9684f1c10083c8c6df845b4a279f03875ddd
Jake Bowen <jakeeb05@gmail.com>
2024-11-02 17:45:18 -0700

    Initial Commit

---
+++ TargetGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TargetGame
+//
+//  Created by Jake Bowen on 11/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
